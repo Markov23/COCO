35d2b3372f07286f451939d7d7f8eaf1d6a3a754
Marco Nava <marcobdac@gmail.com>
2019-11-11 20:50:35 -0600

    Initial commit

---
+++ app/src/main/java/com/example/cocov2/ui/recuperarContrasenaPaso2.java
@@ -0,0 +1,16 @@
+package com.example.cocov2.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cocov2.R;
+
+public class recuperarContrasenaPaso2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recuperar_contrasena_paso2);
+    }
+}
